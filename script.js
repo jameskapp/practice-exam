66d086b4084836cfaf40e98275db6e9262408c6f
James Kapp <42060204+jameskapp@users.noreply.github.com>
2018-11-05 17:17:46 +0200

    added map

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 // use strict;
-let user_input;
+let user_input = [];
 
 console.log("I WORK")
 
 function test() {
   // console.log(`i've been called with: ${input}`)
-  document.write(user_input);
   console.log(user_input);
+  const map_arr = user_input.map(x => document.getElementById("only_one").innerHTML = x);
+  document.getElementById("result").innerHTML = map_arr;
+  // document.getElementById("result").innerHTML = user_input.map(x => <option key={x}>{`${x}`}</option>);
+  console.log("map", map_arr);
 }
 
 function test_change(input) {
-  user_input = input;
+  user_input.push(input);
 }
 
 
