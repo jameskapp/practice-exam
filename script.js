3d56dc43912edba27056b2619abfc63cfd2292e5
James Kapp <42060204+jameskapp@users.noreply.github.com>
2018-11-05 16:03:23 +0200

    added submit

---
+++ script.js
@@ -0,0 +1,19 @@
+// use strict;
+let user_input;
+
+console.log("I WORK")
+
+function test() {
+  // console.log(`i've been called with: ${input}`)
+  document.write(user_input);
+  console.log(user_input);
+}
+
+function test_change(input) {
+  user_input = input;
+}
+
+
+
+
+// test("awe")
